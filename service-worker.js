32473e718b741f0342d69bdbd692b36d6cf5ba39
DataSquirtle <engineering.raul@gmail.com>
2026-05-30 21:29:11 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE = "uvs-calc-v2";
+const CACHE = "uvs-calc-v3";
 const FILES = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/icon.png"
+  "/UniversusCalc/",
+  "/UniversusCalc/index.html",
+  "/UniversusCalc/style.css",
+  "/UniversusCalc/app.js",
+  "/UniversusCalc/manifest.json",
+  "/UniversusCalc/icon.png"
 ];
-
+ 
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -25,7 +25,7 @@ self.addEventListener("activate", e => {
   );
   self.clients.claim();
 });
-
+ 
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
