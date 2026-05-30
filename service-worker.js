e186400e1b2be09bda54d2a571073333e4f6ce22
DataSquirtle <engineering.raul@gmail.com>
2026-05-30 21:32:49 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,14 @@ const FILES = [
   "/UniversusCalc/manifest.json",
   "/UniversusCalc/icon.png"
 ];
- 
+
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
- 
+
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -25,7 +25,7 @@ self.addEventListener("activate", e => {
   );
   self.clients.claim();
 });
- 
+
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
